2ca7a16a5079856b8d3f70c5f5f63197fec515e2
Mat Seto <seanseto@gmail.com>
2021-01-06 19:46:59 -0800

    Updates

--- js/reset.js
+++ js/reset.js
@@ -2,7 +2,8 @@ const resetButton = document.getElementById("reset-button")
 
 resetButton.addEventListener("click", () => {
   store.dispatch(resetState())
-  clearItems()
-
-  initializeFromState(store.getState())
+  const doReload = confirm(
+    "This will restore the default settings and delete all of your modifiers. Continue?",
+  )
+  if (doReload) window.location.reload()
 })
